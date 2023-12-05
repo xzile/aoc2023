fdfd9892fccc53a731111117e9646a4fb06d6cc8
Jonathan Hopson <jonathan.hopson@interfolio.com>
2023-12-05 18:17:29 -0500

    Day 5, optimized

--- 2023/05/code.go
+++ 2023/05/code.go
@@ -20,68 +20,60 @@ func main() {
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
 	lines := strings.Split(strings.ReplaceAll(input, "\r\n", "\n"), "\n")
-	var seedToSoil, soilToFert, fertToWater, waterToLight, lightToTemp, tempToHum, humToLoc []Boundry
+	var seedToSoil, soilToFert, fertToWater, waterToLight, lightToTemp, tempToHum, humToLoc []Boundary
 	if len(lines) < 50 {
 		// Example file
-		seedToSoil = Boundries(lines[3:5])
-		soilToFert = Boundries(lines[7:10])
-		fertToWater = Boundries(lines[12:16])
-		waterToLight = Boundries(lines[18:20])
-		lightToTemp = Boundries(lines[22:25])
-		tempToHum = Boundries(lines[27:29])
-		humToLoc = Boundries(lines[31:33])
+		seedToSoil = Boundaries(lines[3:5])
+		soilToFert = Boundaries(lines[7:10])
+		fertToWater = Boundaries(lines[12:16])
+		waterToLight = Boundaries(lines[18:20])
+		lightToTemp = Boundaries(lines[22:25])
+		tempToHum = Boundaries(lines[27:29])
+		humToLoc = Boundaries(lines[31:33])
 	} else {
-		seedToSoil = Boundries(lines[3:15])
-		soilToFert = Boundries(lines[17:46])
-		fertToWater = Boundries(lines[48:65])
-		waterToLight = Boundries(lines[67:85])
-		lightToTemp = Boundries(lines[87:121])
-		tempToHum = Boundries(lines[123:158])
-		humToLoc = Boundries(lines[160:172])
+		seedToSoil = Boundaries(lines[3:15])
+		soilToFert = Boundaries(lines[17:46])
+		fertToWater = Boundaries(lines[48:65])
+		waterToLight = Boundaries(lines[67:85])
+		lightToTemp = Boundaries(lines[87:121])
+		tempToHum = Boundaries(lines[123:158])
+		humToLoc = Boundaries(lines[160:172])
 	}
 
 	var seeds []string
 	lowestLoc := int64(9999999999999999)
 	if part2 {
-		Log("Using brute force... please wait.")
 		seedDefs := strings.Split(lines[0][7:], " ")
 		for i := 0; i < len(seedDefs); i += 2 {
-			// NOTE: There's probably a much more efficient data structure that computes the breakpoints of the entire "range" of seeds,
-			// where the next seed ultimately changes the location by something non-incrementally.
-			// This would allow you to only check the breakpoints.
-			// To set this up, though, you'd likely need to start at location and work backwards, identifying in the previous step:
-			// - it's own breakpoints
-			// - where in those ranges location changes non-incrementally
-			//
-			// However, brute force only took ~1min 30sec to generate the correct answer, which is faster than I can come up the above.
-
 			seedStart := Atoi(seedDefs[i])
-			// Brute force, checking each seed
 			for j := int64(0); j <= Atoi(seedDefs[i+1]); j++ {
 				seed := seedStart + j
-				soil := Next(seed, seedToSoil)
-				fert := Next(soil, soilToFert)
-				water := Next(fert, fertToWater)
-				light := Next(water, waterToLight)
-				temp := Next(light, lightToTemp)
-				hum := Next(temp, tempToHum)
-				loc := Next(hum, humToLoc)
+				soil, soilSkip := Next(seed, seedToSoil)
+				fert, fertSkip := Next(soil, soilToFert)
+				water, waterSkip := Next(fert, fertToWater)
+				light, lightSkip := Next(water, waterToLight)
+				temp, tempSkip := Next(light, lightToTemp)
+				hum, humSkip := Next(temp, tempToHum)
+				loc, locSkip := Next(hum, humToLoc)
 				if loc < lowestLoc {
 					lowestLoc = loc
 				}
+				// Skip allows us to "jump" ahead an amount of seeds where incrementing by 1 would just produce a location that is 1 more
+				// When j++ is evaluated, the location should be significantly different as the increment will change one or more of the Boundaries that are matched in Next
+				j += j + max(min(soilSkip, fertSkip, waterSkip, lightSkip, tempSkip, humSkip, locSkip)-1, 0)
 			}
 		}
 	} else {
 		seeds = strings.Split(lines[0][7:], " ")
 		for _, seedStr := range seeds {
 			seed := Atoi(seedStr)
-			soil := Next(seed, seedToSoil)
-			fert := Next(soil, soilToFert)
-			water := Next(fert, fertToWater)
-			light := Next(water, waterToLight)
-			temp := Next(light, lightToTemp)
-			hum := Next(temp, tempToHum)
-			loc := Next(hum, humToLoc)
+			soil, _ := Next(seed, seedToSoil)
+			fert, _ := Next(soil, soilToFert)
+			water, _ := Next(fert, fertToWater)
+			light, _ := Next(water, waterToLight)
+			temp, _ := Next(light, lightToTemp)
+			hum, _ := Next(temp, tempToHum)
+			loc, _ := Next(hum, humToLoc)
 			if loc < lowestLoc {
 				lowestLoc = loc
 			}
@@ -91,19 +83,19 @@ func run(part2 bool, input string) any {
 	return lowestLoc
 }
 
-type Boundry struct {
+type Boundary struct {
 	LeftMin int64
 	LeftMax int64
 	Right   int64
 }
 
-func Boundries(lines []string) []Boundry {
-	out := []Boundry{}
+func Boundaries(lines []string) []Boundary {
+	out := []Boundary{}
 	for _, line := range lines {
 		parts := strings.Split(line, " ")
 		// Note: parts are destination/rgt THEN source/lft, and finally the distance/range
 		rgt, lft, dis := parts[0], parts[1], parts[2]
-		out = append(out, Boundry{
+		out = append(out, Boundary{
 			LeftMin: Atoi(lft),
 			LeftMax: Atoi(lft) + Atoi(dis),
 			Right:   Atoi(rgt),
@@ -112,18 +104,22 @@ func Boundries(lines []string) []Boundry {
 	return out
 }
 
-func Next(in int64, boundries []Boundry) int64 {
-	for _, b := range boundries {
-		// If within the boundries of the source/lft
+// Next provides the location of the destination/rgt given an in/source/rgt
+// Additionally, a "skip" is returned, which is the remaining range within the boundary
+func Next(in int64, boundaries []Boundary) (int64, int64) {
+	for _, b := range boundaries {
+		// If within the boundaries of the source/lft
 		if in >= b.LeftMin && in < b.LeftMax {
-			// Compute the difference, so we can add it to the destingation/rgt
+			// Compute the difference, so we can add it to the destination/rgt
 			diff := in - b.LeftMin
-			return b.Right + diff
+			// Skip is the remaining "range" that is just incrementally increasing by 1
+			skip := b.LeftMax - in
+			return b.Right + diff, skip
 		}
 	}
 
 	// If not found, the source and destination are the same
-	return in
+	return in, 0
 }
 
 // Atoi ignores the errors in strconv.Atoi and returns the response
